8d61052acf66e3d92b712a398eba2808203bd431
kxp <andre.cachopas@gmail.com>
2019-04-18 00:30:13 +0100

    middle changes

--- Proto/compile.sh
+++ Proto/compile.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-./protoc -I="." --csharp_out=../ServiceModels/csharp ValidationService.proto UserService.proto
+#./protoc -I="." --csharp_out=../ServiceModels/csharp ValidationService.proto UserData.proto TMAISAPI.proto
+./protoc -I="D:\\projects\\ISD\\Proto\\ValidationModels.proto" --csharp_out=../ServiceModels/csharp ValidationService.proto UserData.proto TMAISAPI.proto
 ./protoc -I="." --java_out=../ServiceModels/java ValidationService.proto UserService.proto
 ./protoc -I="." --cpp_out=../ServiceModels/cpp ValidationService.proto UserService.proto
+
+
+./protoc  --include_imports -I="D:\\projects\\ISD\\Proto\\ValidationModels.proto" --csharp_out=../ServiceModels/csharp ValidationService.proto UserData.proto TMAISAPI.proto
+
+
+ ./protoc  --include_imports -I="../../Proto/ValidationModels.proto" --csharp_out=../ServiceModels/csharp ValidationService.proto UserData.proto TMAISAPI.proto
--- RemoteServiceServer/RemoteServiceServer/ValidationServiceImpl.cs
+++ RemoteServiceServer/RemoteServiceServer/ValidationServiceImpl.cs
@@ -10,12 +10,7 @@ namespace RemoteServiceServer
 
         public ValidationServiceImpl()
         {
-            var server = new Server
-            {
-                Services = { ValidationService.ValidationServiceReflection.BindService(new ValidationServiceImpl()) },
-                Ports = { new ServerPort("127.0.0.1", 5000, ServerCredentials.Insecure) }
-            };
-            server.Start();
+          
         }
     }
 }
