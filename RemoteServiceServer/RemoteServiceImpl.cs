9a32ab35994089d962b9116c17bc0d3bcc7a92bf
kxp <andre.cachopas@gmail.com>
2019-05-08 23:27:45 +0100

    Initial implementation of the grpc

---
+++ RemoteServiceServer/RemoteServiceImpl.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using ValidationModels;
+
+namespace RemoteServiceServer
+{
+
+
+    public class ValidationServiceImpl : ValidationService.ValidationService.ValidationServiceBase
+    {
+        public ValidationServiceImpl()
+        {
+
+        }
+
+        public override Task<ValidationReply> ValidationSingle(ValidationRequest request, ServerCallContext context)
+        {
+
+            Console.WriteLine("{0}", request.CodeCSMP);
+
+            //context.WriteResponseHeadersAsync()
+            
+            //not implemented
+            //context.Status = Status.DefaultSuccess;
+
+            return base.ValidationSingle(request, context);
+        }
+
+
+        public override Task ValidationStreaming(IAsyncStreamReader<ValidationRequest> requestStream, IServerStreamWriter<ValidationReply> responseStream,
+            ServerCallContext context)
+        {
+            return base.ValidationStreaming(requestStream, responseStream, context);
+        }
+    }
+
+}
