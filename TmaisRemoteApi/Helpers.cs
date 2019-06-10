713d3532c423056592205b49a14f81d350c6e561
kaxopas <andre.cachopas@gmail.com>
2019-06-10 17:37:47 +0100

    Added configs

--- TmaisRemoteApi/Helpers.cs
+++ TmaisRemoteApi/Helpers.cs
@@ -1,7 +1,18 @@
 ﻿namespace TmaisRemoteApi
 {
+
+
     public class Helpers
     {
-        
+
+        public static string  MsgConnection;
+
+        public static void ReadConfigs()
+        {
+            MsgConnection = ConfigurationManager.AppSettings["msg-ip"];
+            //UserIBAN = ConfigurationManager.AppSettings["user-iban"];
+            //RemoteAPI = ConfigurationManager.AppSettings["remote-api"];
+
+        }
     }
 }
